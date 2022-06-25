c817af64c66838df2f80a40c42580646c5a93bff
nneia <bazhirov.work@gmail.com>
2022-06-25 20:06:33 +0900

    interactive hover shade on menu

--- main-scripts.js
+++ main-scripts.js
@@ -11,22 +11,31 @@ let title = document.querySelector(".title");
 let menu_items = document.querySelector(".menu-container");
 let scoll_past = document.querySelector('.wlcm-container');
 
-console.log(height);
+
+function add_shade() {
+	height.classList.add("upd-height");
+    shade.classList.add("upd-shade");
+    title.classList.add("upd_title");
+    menu_items.classList.add("upd_items");
+}
+function remove_shade() {
+	height.classList.remove("upd-height");
+    shade.classList.remove("upd-shade");
+    title.classList.remove("upd_title");
+    menu_items.classList.remove("upd_items");
+}
 
 window.onscroll = function(){
-    //TOP
-    if(window.scrollY > 835){
-        console.log("TRIGGER: top of div reached.");
-        height.classList.add("upd-height");
-        shade.classList.add("upd-shade");
-        title.classList.add("upd_title");
-        menu_items.classList.add("upd_items");
+
+    if(window.scrollY > 780){
+        add_shade();
     }
-    if(window.scrollY < 835){
-        console.log("TRIGGER: top of div reached.");
-        height.classList.remove("upd-height");
-        shade.classList.remove("upd-shade");
-        title.classList.remove("upd_title");
-        menu_items.classList.remove("upd_items");
+    if(window.scrollY < 780){
+        remove_shade();
     }
 }
+
+function hovered() {
+    shade.classList.toggle("upd-shade");
+}
+
